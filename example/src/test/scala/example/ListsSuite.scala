b34749158ea3c8fe39263e3efcffc9a00d11c92b
lucas <lucas1000001@gmail.com>
2013-09-18 20:52:35 +0100

    Setup of second project

--- example/src/test/scala/example/ListsSuite.scala
+++ example/src/test/scala/example/ListsSuite.scala
@@ -47,7 +47,7 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2)
   }
 
 
@@ -72,7 +72,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2)
   }
 
 
@@ -117,8 +117,22 @@ class ListsSuite extends FunSuite {
   test("sum of a few numbers") {
     assert(sum(List(1,2,0)) === 3)
   }
+
+  test("sum with negatives") {
+    assert(sum(List(-1,0,1)) === 0)
+  }
   
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
+  }
+
+  test("max of negatives") {
+    assert(max(List(-3,-2,-1)) === -1)
+  }
+
+  test("no max in empty list") {
+    intercept[NoSuchElementException] {
+      max(List())
+    }
   }
 }
